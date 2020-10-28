63ff2ac525a6bfd4c0f018eb00a0fea35e4cd122
canberk <canberkkoc.94@gmial.com>
2020-10-28 12:39:45 +0300

    svg install

---
+++ src/components/CustomText.vue
@@ -0,0 +1,39 @@
+<template>
+    <component :is="tag" :class="size">
+        <slot />
+    </component>
+</template>
+
+<script>
+export default {
+    name:"CustomText",
+    props: {
+        tag:{
+            type:String,
+            default:'span'
+        },
+        size:{
+            type:String,
+            default:"normal" ,// base
+            validator: function(value){
+                return ["normal","small","xsmall","xxsmall"].indexOf(value) !== -1
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.normal{
+    font-size: 14px;
+}
+.small{
+    font-size: 12px;
+}
+.xsmall{
+    font-size: 11px;
+}
+.xxsmall{
+    font-size: 10px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">  
+   <CustomText class="a1">hello</CustomText>
+   <CustomText class="a2" tag="p" size="small">hello</CustomText>
+   <CustomText class="a3" tag="p" size="xsmall">hello</CustomText>
+   <CustomText class="a4" tag="p" size="xxsmall">hello</CustomText>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CustomText from '@/components/CustomText'
+  import '@/style/app.css';
+
+export default {
+  name: "Home",
+  components:{
+     CustomText
+  }
+ 
+};
+</script>
+
+<style scoped>
+.a1{
+   color:red;
+ }
+ .a2{
+   color:rgb(var(--d62))
+ }
+ .a3{
+   color:rgb(var(--i1d),.6)
+ }
+ .a4{
+   color:rgb(var(--b86))
+ }
+</style>
+ 
+
